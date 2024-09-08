d281c69944c5c27ef70999c25455a2675eca2d24
Zachary Gibson <zackgib@aol.com>
2024-09-08 08:28:32 -0500

    Initial Commit

---
+++ MetalPetal HDR Playground/MetalPetal_HDR_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalPetal_HDR_PlaygroundApp.swift
+//  MetalPetal HDR Playground
+//
+//  Created by Zachary Gibson on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalPetal_HDR_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
